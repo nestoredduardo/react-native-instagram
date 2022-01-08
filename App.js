3801cf279a583ee7c9296c2d08449a3a5fa84665
Nestor Mamani <nmamanipantoja@gmail.com>
2022-01-08 12:06:56 -0500

    Integrate TailwindCSS

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import tailwind from 'tailwind-rn';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tailwind('h-full flex')}>
+      <Text style={tailwind('bg-pink-200 text-red-500 font-bold')}>
+        Hello World
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); */
